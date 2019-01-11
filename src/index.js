830a6f3af5e5a5d719e86ca39f8aab110bddc788
Alec Levin <arl505@humboldt.edu>
2019-01-10 23:03:06 -0800

    sucessfully installed reactstrap and setup sandbox to develop in

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './welcomePage/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// tbh idk what the code below does or what the corresponding  
+// serviceWorker.js file do, but I'm going to leave them, don't ask why
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
