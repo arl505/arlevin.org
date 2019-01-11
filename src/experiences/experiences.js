15d143606b969ccd2457f1c3535db7771b1d23a6
Alec Levin <arl505@humboldt.edu>
2019-01-10 23:39:05 -0800

    implemented routing and setup sandboxes for academics, experiences, and welcome page

---
+++ src/experiences/experiences.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+
+class Experiences extends Component {
+    render() {
+        return (
+            <Button color="danger">Experiences</Button>
+        )
+    }
+}
+
+export default Experiences;
