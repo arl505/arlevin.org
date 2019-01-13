b6392527487ca0199960fb9e248024501602b28b
Alec Levin <arl505@humboldt.edu>
2019-01-12 22:25:57 -0800

    implemented my first styled-component💅, had my first go at using props within the styled component and then changing the props using the state, much success

--- src/navBar/navBar.js
+++ src/navBar/navBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, NavLink} from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch, NavLink} from "react-router-dom";
 import Home from '../welcomePage/home';	
 import Academics from '../academics/academics';	
 import Experience from '../experience/experience';	
@@ -10,23 +10,69 @@ import ReactDOM from 'react-dom';
 import {Navbar, Button, NavbarBrand, Nav, NavItem, Popover, PopoverBody, PopoverHeader} from 'reactstrap';
 import Fade from 'react-reveal/Fade';
 import { slide as Menu } from "react-burger-menu";
+import HoverImage from "react-hover-image";
+import gitLogo from "./github-logo.png";
+import gitLogoBlue from "./github-logo-blue.png";
+import emailLogo from "./email-logo.png";
+import emailLogoBlue from "./email-logo-blue.png";
+import linkedinLogo from "./linkedin-logo.png";
+import linkedinLogoBlue from "./linkedin-logo-blue.png";
+import styled from 'styled-components'
 
-class NavBar extends Component {
+const MyNavLink = ({className, children}) => (
+  <NavLink to="/" className={className}> {children}</NavLink>
+);
+
+
+const StyledNavLink = styled(MyNavLink)`
+    color: ${props => props.inputColor || "#B8B8B8"};
+    margin: 5px;
+    text-decoration: none;
+    font-weight: lighter;
+    padding-bottom: 1.25em;
+    padding-top: 1.25em;
+    padding-left: .5em;
+    padding-right: .5em
+    &:hover {
+      color: #E8E8E8;
+      text-decoration: none;
+    }
+`;
+
+class NavBar extends Component { 
 
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
-    this.state = {
-      width: window.innerWidth,
-
-      popoverOpen: false
-    };
+      this.state = {
+        width: window.innerWidth,
+        popoverOpen: false,
+        homeColor: "#B8B8B8"
+       }
   }
+
+
+componentDidMount(){
+  var url = window.location.href;
+  if(url.includes("contact"))
+  {
+    this.setState({
+      popoverOpen: true
+    });
+    isHome = true;
+  }
+}
 
   toggle() {
     this.setState({
       popoverOpen: !this.state.popoverOpen
+    });
+  }
+
+  closePopover() {
+    this.setState({
+      popoverOpen: false
     });
   }
 
@@ -42,6 +88,7 @@ class NavBar extends Component {
   closeMenu () {
     this.setState({menuOpen: false})
   }
+
   
   // make sure to remove the listener
   // when the component is not mounted anymore
@@ -53,59 +100,93 @@ class NavBar extends Component {
     this.setState({ width: window.innerWidth });
   };
 
+  componentDidUpdate(){
+    var url = window.location.href;
+    if(url.includes("contact")){
+      if(this.state.homeColor !== "#FFFFFF")
+      {
+        this.setState({
+          homeColor: "#FFFFFF",
+         });
+      }
+    }
+    else if(url.includes("about") || url.includes("academics") || url.includes("experience")){
+      if(this.state.homeColor !== "#B8B8B8")
+      {
+        this.setState({
+          homeColor: "#B8B8B8",
+         });
+      }
+    }
+    else{
+      if(this.state.homeColor !== "#FFFFFF")
+      {
+        this.setState({
+          homeColor: "#FFFFFF",
+         });
+      }
+    }
+    console.log(this.state.homeColor);
+
+  }
+
   render() {
     const { width } = this.state;
     const isMobile = width <= 651;
     let nav;
     if(!isMobile){
       nav = (<Fade>
-        <div className="backdrop" style={{"minHeight":"100vh", "height":"100"}}>
+        <div className="backdrop" style={{"minHeight":"100vh", width:"100vw", "height":"100"}}>
         <BrowserRouter>
         <div>
           <div className="NavBar">
-            <Navbar >
-                <NavbarBrand className="NavbarBrand" tag={NavLink} to="/">Alec Levin</NavbarBrand>
+            <Navbar onClick={() => this.closePopover()}>
+                <NavbarBrand onClick={() => this.closePopover()} className="NavbarBrand" tag={NavLink} to="/" >Alec Levin</NavbarBrand>
                 <Nav>
-                
-                <NavItem>
-                  <NavLink to="/" id="homeTab" exact activeClassName="activeTab" className="NavbarItems">Home</NavLink>
-                </NavItem>
+              <NavItem>
+                <StyledNavLink to="/" id="homeTab" exact inputColor={this.state.homeColor}>Home</StyledNavLink>
+              </NavItem>
 
-                  <NavItem>
+                  <NavItem onClick={() => this.closePopover()}>
                     <NavLink to="/about" activeClassName="activeTab" id="aboutTab" className="NavbarItems">About</NavLink>
                   </NavItem>
-                  <NavItem>
+
+                  <NavItem onClick={() => this.closePopover()}>
                     <NavLink to="/academics" activeClassName="activeTab" id="academicsTab" className="NavbarItems">Academics</NavLink>
                   </NavItem>
 
-                  <NavItem>
+                  <NavItem onClick={() => this.closePopover()}>
                     <NavLink to="/experience" activeClassName="activeTab" id="ExperienceTab" className="NavbarItems">Experience</NavLink>
                   </NavItem>
 
                   <NavItem>
                   <Button id="PopoverFocus" style={{padding:"0em", margin:"0em", border:"none", backgroundColor:"transparent"}}>
-                    <NavLink to="/contact" onClick={e => e.preventDefault()} activeClassName="activeTab" id="Popover1" className="NavbarItems">Contact & More</NavLink>
+                    <NavLink to="/contact" onClick={e => e.preventDefault()}  id="Popover1" className="NavbarItems">Contact</NavLink>
                     </Button>
-                    <Popover trigger="focus" placement="bottom" isOpen={this.state.popoverOpen} target="PopoverFocus" toggle={this.toggle}>
-                      <PopoverHeader>Popover Title</PopoverHeader>
-                      <PopoverBody>Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</PopoverBody>
+                    <Popover style={{textAlign:"center"}} placement="bottom" isOpen={this.state.popoverOpen} target="PopoverFocus" toggle={this.toggle}>
+                      <PopoverHeader>Connect with me</PopoverHeader>
+                      <PopoverBody>
+                      <div style={{textAlign:"center"}}>
+                            <a href="https://github.com/arl505"><HoverImage className="logo" style={{width:"40px", height:"40xp", margin:"1em"}} src={gitLogo} hoverSrc={gitLogoBlue}/></a>
+                            <a href={"mailto:levin.alec@gmail.com?subject=Let's work together!"}><HoverImage className="logo" style={{width:"40px", height:"40xp", margin:"1em"}} src={emailLogo} hoverSrc={emailLogoBlue}/></a>
+                            <a href="https://www.linkedin.com/in/alec-levin/"><HoverImage className="logo" style={{width:"40px", height:"40xp", margin:"1em"}} src={linkedinLogo} hoverSrc={linkedinLogoBlue}/></a>
+                        </div>
+                      </PopoverBody>
                     </Popover>
 
                   </NavItem>
                 </Nav>
             </Navbar>
-
-          
-
             </div>
           
-          <div>
+          <div onClick={() => this.closePopover()}>
           <Switch>
+           {/* <Redirect from='/contact' to='/'/> */}
+            <Route path="/contact" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/academics" component={Academics}/>
             <Route path="/experience" component={Experience}/>
-            {/*<Route path="/contact" component={Contact}/>*/}
-            <Route path="/"  component={Home}/> {/* this line comes last and will be the default (or no matches)*/}
+            <Route path="/" component={Home}/> {/* this line comes last and will be the default (or no matches)*/}
           </Switch>
           </div>
           </div>
@@ -117,7 +198,7 @@ class NavBar extends Component {
     else{
       nav = (<BrowserRouter>
         <div id="App">
-        <input type="text" autofocus="autofocus" style={{"display":"none"}} />
+        <input type="text" autoFocus="autofocus" style={{"display":"none"}} />
         <Menu isOpen={this.state.menuOpen}
           onStateChange={(state) => this.handleStateChange(state)}>
           <NavItem className="noBox" tabIndex="-1">
@@ -165,6 +246,7 @@ class NavBar extends Component {
   }
 }
 
+var isHome = false;
 
 ReactDOM.render(
   <NavBar />,
