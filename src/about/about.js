ed0cc0b402ab20be6cc3a48ab7e9e3573e3865f3
Alec Levin <arl505@humboldt.edu>
2019-01-14 23:24:17 -0800

    began about tab: made a photo collage of projects. Added improved meta tags for Google/Linkedin etc. Added temporary picture of myself

--- src/about/about.js
+++ src/about/about.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
 import ReactDOM from 'react-dom';
 import Fade from 'react-reveal/Fade'
 import ReactGA from 'react-ga';
+import { Media } from 'reactstrap';
+import Project_collage from "./project-collage.png"
 
 
 class About extends Component {
@@ -17,11 +18,11 @@ class About extends Component {
     render() {
         return (
             <Fade>
-                <div style={{position:"absolute",width:"100%", height:"100%"}}>
-                    <div style={{ textAlign:"center", marginTop:"50vh" }}>
-                    <Button color="danger">About</Button>
+                    <div style={{marginTop:"5.5em"}}>
+                        <div style={{textAlign:"center"}}>
+                          <Media object src={Project_collage} alt="projects" style={{maxWidth:"98vw", maxHeight:"40vh"}}/>
+                        </div>
                     </div>
-                </div>
             </Fade>
         )
     }
