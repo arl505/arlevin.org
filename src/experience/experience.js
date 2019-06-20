cb5ed8338d6a87ce2483a0f6439074b69ad1f85b
Alec Levin <arl505@humboldt.edu>
2019-06-19 22:04:39 -0400

    updated experience

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -20,16 +20,25 @@ class Experience extends Component {
                     <ul className="myList">
                         <li className="myList"><h3>Work</h3></li>
                         <ul className="myList">
+
+                            <li className="myList">Ford Motor Company - Full Stack Software Engineer (June 2019-Present)
+                                <ul className="myListWithBullets">
+                                    <li className="myListWithBullets">Member of the Connected Vehicle Platform and Products (CVPP) group within the Smart Mobility division</li>
+                                    <li className="myListWithBullets">Focused on using Java to create and deploy highly available and highly scalable APIs and applications that receive millions of requests per day</li>
+                                    <li className="myListWithBullets">Projects spaned from building mechanisms that allow users to connect to third party applications (navigation, etc.) to building systems which support future autonomous mobility</li>
+                                </ul>
+                            </li>
+
                             <li className="myList">Codepath.org - Tech Fellow (October 2018-May 2019)
                                 <ul className="myListWithBullets">
                                     <li className="myListWithBullets">Served as an instructor for the Android Applications course at Humboldt State University</li> 
                                     <li className="myListWithBullets">Required a high degree of understanding of the curriculum topics, including but not limited to: Java, C#, Android Studio, Visual Studio, Gradle, Xamarin, (A)XML, and Git</li>
                                     <li className="myListWithBullets">Responsible for teaching the for-credit course to roughly 15 students across a 15 week course with each week comprising of two lectures and a lab</li>
-                                    <li className="myListWithBullets">Organized meetings, assignments, lectures, labs, and grading.</li>
+                                    <li className="myListWithBullets">Organized meetings, assignments, lectures, labs, and grading</li>
                                 </ul>
                             </li>
 
-                            <li className="myList">Planet Rocket - Full Stack Web Engineer (March 2018-Present)
+                            <li className="myList">Planet Rocket - Full Stack Web Engineer (March 2018-May 2019)
                                 <ul className="myListWithBullets">
                                     <li className="myListWithBullets">Led redesigning and updating of previous <a href="https://pr1.arlevin.org">V1</a> codebase on LAMP (Linux, Apache, MySQL, and PHP) stack</li>
                                     <li className="myListWithBullets">Designed and implemented <a href="https://pr2.arlevin.org">V2</a> with a MERN (MongoDB, Express.js, React.js, and Node.js) stack deployed on an AWS Elastic Cloud Compute (EC2) instance serving the project with NGINX</li>
